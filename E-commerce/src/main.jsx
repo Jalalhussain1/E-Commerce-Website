9d0dfd5856d0496ef051e0acf8ae2c5a5d5e4950
JALAL HUSSAIN <125751908+Jalalhussain1@users.noreply.github.com>
2024-11-27 19:18:52 +0500

    some problem in files and reslove the problem

--- E-commerce/src/main.jsx
+++ E-commerce/src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client'; // Correct import for createRoot
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import ShopContextProvider from './contex/ShopContex.jsx';
+import ShopContextProvider from './context/ShopContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
